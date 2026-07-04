33eea530a5edff3018c6d238c83259018705e22d
Shahnawaz Shaikh <shahnawazshaikh67967@gmail.com>
2026-07-05 01:44:50 +0530

    Dutuch National Flag  Algo done

---
+++ Arrays/dutuchnationalflagalogo.js
@@ -0,0 +1,32 @@
+arr = [2,0,1,2,1,0];
+function sortColors(arr) {
+
+    let low = 0;
+    let mid = 0;
+    let high = arr.length - 1;
+
+    while (mid <= high) {
+
+        if (arr[mid] === 0) {
+
+            [arr[low], arr[mid]] = [arr[mid], arr[low]];
+
+            low++;
+            mid++;
+
+        } else if (arr[mid] === 1) {
+
+            mid++;
+
+        } else {
+
+            [arr[mid], arr[high]] = [arr[high], arr[mid]];
+
+            high--;
+        }
+    }
+
+    return arr;
+}
+
+console.log(sortColors(arr));
